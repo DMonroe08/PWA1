131b19c1189cc7d408a8f3278d77a109eb33fa0f
dmonroe08 <dmonroe08@fullsail.edu>
2014-04-18 15:01:49 -0400

    button and students added

---
+++ Week 3/goal6_assign_midterm/js/main.js
@@ -0,0 +1,32 @@
+/*
+	* Mid Terms for PWA-1
+	* Danielle Monroe
+*/
+
+(function)(){
+    var button = document.getElementById(".buttonred");
+    var i = 0;
+
+
+    var students = [
+
+        {
+            name: "John Lennon",
+            address: {'street': "23 Abbey Lane", 'city': "London", 'state': "New York"},
+            gpa: [2.3, 2.2, 3.5, 2.8]
+        },
+
+        {
+            name: "Bruno Mars",
+            address: {'street': "48 Empire Street", 'city': "Rome", 'state': "Kansas"},
+            gpa: [3.5, 4.0, 2.6,3.4]
+
+        },
+        {
+            name:"Audry Hepburn",
+            address: {'street': "19 Tiffany Blvd", 'city': "Higgans", 'state': "California"},
+            gpa: [4.0, 2.8, 2.6, 3.8]
+
+        }]
+
+}
